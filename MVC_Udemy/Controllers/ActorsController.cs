c702f2ba87d4294f5862aa28ef5c91a3f98d324e
Daniel Hernandez Talla <hernandezdany41@gmail.com>
2021-09-19 08:50:11 -0700

    Adding Actors Controller

---
+++ MVC_Udemy/Controllers/ActorsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Udemy.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Udemy.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ActorsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var data = _context.Actors.ToList();
+            return View();
+        }
+    }
+}
