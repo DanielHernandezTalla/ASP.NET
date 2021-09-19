cc5eb9de80575657e8148186eaa7167e4d2f0576
Daniel Hernandez Talla <hernandezdany41@gmail.com>
2021-09-19 08:59:56 -0700

    Adding movies contoller

---
+++ MVC_Udemy/Controllers/MoviesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVC_Udemy.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Udemy.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public MoviesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allMovies = await _context.Movies.ToListAsync();
+            return View();
+        }
+    }
+}
