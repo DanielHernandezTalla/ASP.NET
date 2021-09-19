d2618a0709b6b4201373721f0e215c7d1816e8c1
Daniel Hernandez Talla <hernandezdany41@gmail.com>
2021-09-19 08:57:02 -0700

    Adding cinemas and producers controllers

---
+++ MVC_Udemy/Controllers/CinemasController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVC_Udemy.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Udemy.Controllers
+{
+    public class CinemasController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CinemasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allCinemas = await _context.Cinemas.ToListAsync();
+            return View();
+        }
+    }
+}
