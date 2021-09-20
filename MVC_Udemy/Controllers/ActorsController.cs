65eee554e6d96d4afd6961996a608b66f60f7a9b
Daniel Hernandez Talla <hernandezdany41@gmail.com>
2021-09-19 20:16:54 -0700

    Adding details in ActorsController

--- MVC_Udemy/Controllers/ActorsController.cs
+++ MVC_Udemy/Controllers/ActorsController.cs
@@ -20,7 +20,7 @@ namespace MVC_Udemy.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var data = await _service.GetAll();
+            var data = await _service.GetAllAsync();
             return View(data);
         }
 
@@ -38,8 +38,19 @@ namespace MVC_Udemy.Controllers
             {
                 return View(actor);
             }
-            _service.Add(actor);
+            await _service.AddAsync(actor);
             return RedirectToAction(nameof(Index));
+        }
+
+        // Get: Actors/Details/1
+        public async Task<IActionResult> Details(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+
+            if (actorDetails == null) return View("Empty");
+
+            return View(actorDetails);
+
         }
     }
 }
