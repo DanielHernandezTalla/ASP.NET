98c1722e9e3d3a5234c0750141a899ab53a7927f
Daniel Hernandez Talla <hernandezdany41@gmail.com>
2021-10-13 11:25:35 -0700

    Adding ViewComponent ShoppingCartSummary

---
+++ MVC_Udemy/Data/ViewComponents/ShoppingCartSummary.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Udemy.Data.Cart;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Udemy.Data.ViewComponents
+{
+    public class ShoppingCartSummary: ViewComponent
+    {
+        private readonly ShoppingCart _service;
+
+        public ShoppingCartSummary( ShoppingCart service)
+        {
+            _service = service;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = _service.GetShoppingCartItems();
+
+            return View(items.Count);
+        }
+    }
+}
